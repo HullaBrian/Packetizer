ffb9939cc49c452578a6b519d7f3a5ac2a3e8de5
hullabrian <Beierlejc@gmail.com>
2022-12-27 21:06:04 -0600

    Encoding mostly done

--- packetizer/__init__.py
+++ packetizer/__init__.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
 import _io
+import binascii
 
 from loguru import logger
 
+from packetizer.helpers import Exceptions
 from packetizer.helpers import (
     _check_packet_format,
-    _check_data_with_template
+    _check_data_with_template,
+    PacketField
 )
 
 
 def encode(template: str | _io.TextIOWrapper, data: bytes) -> bytes:
     if type(template) is not _io.TextIOWrapper:
-        template = open(template)
+        template = open(template, "r")
 
     lines = template.readlines()
     logger.info("Verifying data and template structure/format...")
@@ -21,11 +24,32 @@ def encode(template: str | _io.TextIOWrapper, data: bytes) -> bytes:
     except Exception as e:
         logger.critical("Something went wrong with verifying the integrity of the template/data!")
         return False
-
     logger.success("Verified data/template structure!")
 
+    packet_template = {}
     for line in lines:
-        pass
+        split_line = line.split(":")
+        packet_template[split_line[0]] = PacketField(
+            split_line[0],
+            split_line[1],
+            split_line[2]
+        )
+
+    out: bytes = b""
+    for field in data.keys():
+        value = data[field]
+
+        if type(value) is str and not value.startswith("0x"):
+            out += bytes(value, "utf-8")
+        elif type(value) is int:
+            try:
+                out += value.to_bytes(int(packet_template[field].data_size), 'big')
+            except ValueError:
+                out += value.to_bytes(int(data[field]), 'big')
+        elif type(value) is str:  # hex value
+            out += binascii.unhexlify(value.replace("0x", ""))
+
+    return out
 
 
 def decode(template: str | _io.TextIOWrapper, data: bytes) -> dict:
--- packetizer/examples/encode.py
+++ packetizer/examples/encode.py
@@ -1,11 +1,12 @@
-from packetizer import encode, decode
+from packetizer import encode
 
 
 data = {
         "FIRST_FIELD": 100,
         "SECOND_FIELD": "TEST",
-        "THIRD_FIELD_LENGTH": 4,
+        "THIRD_FIELD_LENGTH": 20,
         "FOURTH_FIELD": "Yep",
-        "FIFTH_FIELD": int('420', base=16)
+        "FIFTH_FIELD": '0xfef00a01'
 }
 encoded_packet = encode(template=open("example.packet", "r"), data=data)
+print(encoded_packet)
